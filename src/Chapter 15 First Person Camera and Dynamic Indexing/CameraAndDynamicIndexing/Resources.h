65adfbb84d436f2bf8e44364fe36e1f12a2da1fa
sherlockcai <sherlockcai@tencent.com>
2023-02-17 17:51:40 +0800

    add macro to avoid writing boilerplate in order to create dds textures

--- src/Chapter 15 First Person Camera and Dynamic Indexing/CameraAndDynamicIndexing/Resources.h
+++ src/Chapter 15 First Person Camera and Dynamic Indexing/CameraAndDynamicIndexing/Resources.h
@@ -10,4 +10,29 @@ namespace Resources
     
     static t_TextureArray CharacterTextures {};
     static t_TextureLookup RegularTextures {};
+
+/**
+ * to use a handy LoadTexture macro to eliminate writing boilerplate,
+ * you will have to follow following rules:
+ * 1. put texture file under "../../Textures/" folder
+ * 2. name the texture as "T_{#id}.dss"
+ *    where {#id} should be substituted by id that you input as parameter
+ */
+#define LoadTexture(id) \
+    LoadTextureByPath(id, std::wstring(L"../../Textures/T_") + std::wstring(L#id) + std::wstring(L".dds"))
+
+/**
+ * filePath should be std::wstring family
+ */
+#define LoadTextureByPath(id, filePath) {                   \
+    auto (id) = std::make_unique<Texture>();                \
+    (id)->Name = #id;                                       \
+    (id)->Filename = filePath;                        \
+    ThrowIfFailed(DirectX::CreateDDSTextureFromFile12(      \
+        md3dDevice.Get(),                                   \
+        mCommandList.Get(), (id)->Filename.c_str(),         \
+        (id)->Resource, (id)->UploadHeap));                 \
+    Resources::RegularTextures[(id)->Name] = std::move(id); \
+    }
+    
 }
