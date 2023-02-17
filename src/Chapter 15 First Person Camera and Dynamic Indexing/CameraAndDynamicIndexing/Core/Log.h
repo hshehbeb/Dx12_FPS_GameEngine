1de48028ef4faf36e41aec341fadbb87c88bdb74
sherlockcai <sherlockcai@tencent.com>
2023-02-17 14:11:33 +0800

    feat: add Log

---
+++ src/Chapter 15 First Person Camera and Dynamic Indexing/CameraAndDynamicIndexing/Core/Log.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <string>
+#include <fstream>
+#include <iostream>
+
+namespace Log
+{
+#define LOG_PATH "C:/Users/sherlock/Desktop/log.txt"
+    
+    void inline Debug(const std::string& msg)
+    {
+        std::ofstream f {};
+        f.open(LOG_PATH, std::ios_base::app);
+        f << "[DEBUG]" << msg << std::endl;
+        f.close();
+    }
+}
