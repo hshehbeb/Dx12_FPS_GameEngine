9b3aa42334b69c256d06097e07c4d94a1c7d10bc
sherlockcai <sherlockcai@tencent.com>
2023-03-17 11:02:23 +0800

    add new component Transform

---
+++ src/Chapter 15 First Person Camera and Dynamic Indexing/CameraAndDynamicIndexing/Core/Components/Transform.cpp
@@ -0,0 +1,54 @@
+﻿#include "Transform.h"
+
+Transform::Transform(XMFLOAT3 pos, XMFLOAT3 scale, XMFLOAT3 rot)
+{
+    SetPosition(pos);
+    SetRotation(rot);
+    SetScale(scale);
+}
+
+void Transform::Initialize(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList, RenderItemsList& rItemsList)
+{
+}
+
+void Transform::Update(Actor* owner)
+{
+}
+
+void Transform::SetPosition(XMFLOAT3 newPos)
+{
+    mPosition_f3   = newPos;
+    mPosition_Matr = XMMatrixTranslation(newPos.x, newPos.y, newPos.z);
+}
+
+void Transform::SetRotation(XMFLOAT3 newEulerAngle)
+{
+    mRotation_f3   = newEulerAngle;
+    mRotation_Matr = XMMatrixRotationRollPitchYaw(newEulerAngle.x, newEulerAngle.y, newEulerAngle.z);
+}
+
+void Transform::SetScale(XMFLOAT3 newScale)
+{
+    mScale_f3   = newScale;
+    mScale_Matr = XMMatrixScaling(newScale.x, newScale.y, newScale.z);
+}
+
+XMMATRIX Transform::GetWorldMatrix() const
+{
+    return mScale_Matr * mRotation_Matr * mPosition_Matr;
+}
+
+XMFLOAT3 Transform::GetPosition()
+{
+    return mPosition_f3;
+}
+
+XMFLOAT3 Transform::GetRotation()
+{
+    return mRotation_f3;
+}
+
+XMFLOAT3 Transform::GetScale()
+{
+    return mScale_f3;
+}
---
+++ src/Chapter 15 First Person Camera and Dynamic Indexing/CameraAndDynamicIndexing/Core/Components/Transform.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "IComponent.h"
+
+class Transform : public IComponent
+{
+public:
+    Transform(
+        XMFLOAT3 pos = {0, 0, 0},
+        XMFLOAT3 scale = {1, 1, 1},
+        XMFLOAT3 rot = {MathHelper::Pi / 2, 0, 0}
+    );
+
+    void Initialize(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList, RenderItemsList& rItemsList) override;
+    void Update(Actor* owner) override;
+
+    void SetPosition(XMFLOAT3 newPos);
+    void SetRotation(XMFLOAT3 newEulerAngle);
+    void SetScale(XMFLOAT3 newScale);
+
+    XMMATRIX GetWorldMatrix() const;
+    XMFLOAT3 GetPosition();
+    XMFLOAT3 GetRotation();
+    XMFLOAT3 GetScale();
+
+private:
+    XMFLOAT3 mPosition_f3;
+    XMFLOAT3 mRotation_f3;
+    XMFLOAT3 mScale_f3;
+    XMMATRIX mPosition_Matr;
+    XMMATRIX mRotation_Matr;
+    XMMATRIX mScale_Matr;
+};
